53720e06146928d8cfc41bcc54ef3527acc20fa3
PabloDeWulf <wulfuspablo@gmail.com>
2023-02-22 11:40:28 +0100

    Random commit van Pablo

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=pingpongtoren-noPI-main
+PROJECTNAME=pingpongtoren
 
 # Active Configuration
 DEFAULTCONF=default
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=pingpongtoren-noPI-main.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/pingpongtoren-noPI-main.production.hex
+CND_ARTIFACT_NAME_default=pingpongtoren.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pingpongtoren.production.hex
